bfaca3075c462edcfbe213bc822e958b2fd7519b
zihan20120617 <zihan20120617@gmail.com>
2026-09-19 12:47:10 +0900

    78

--- UNIgame/Assets/Spricts/UboatMoverSprict.cs
+++ UNIgame/Assets/Spricts/UboatMoverSprict.cs
@@ -7,18 +7,35 @@ public class UboatMoverSprict : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("ee");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeydown(keycode.leftarrow)){
-            transform.position += new Vector3(0,0,-10);
+        if(Input.GetKeyDown(KeyCode.LeftArrow)){
+            if(transform.position.x > -9 ){
+                transform.position += new Vector3(-10,0,0);
+
+            }
+            
         }
 
-        if(Input.Getkeydown(Keycode.rightarrow)){
-            transform.position += new Vector3(0,0,10);
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            if(transform.position.x < 9 ){
+                transform.position += new Vector3(10,0,0);
+
+            }
+            
         }
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("ee");
+        
+           
+        
+    }
+
 }
